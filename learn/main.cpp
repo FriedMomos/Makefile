d87b575c6e84bdbb89926229e20e8ae4eee9ffbe
FriedMomos <tanish054@outlook.com>
2024-06-21 15:31:36 +0530

    update

--- learn/main.cpp
+++ learn/main.cpp
@@ -3,8 +3,8 @@
 #include "new.h"
 
 int main(){
-int a=5,b=4;
-int result = add(a,b);
-std::cout<<"The result = "<<result<<std::endl;
-return 0;
+    int a=5,b=4;
+    int result = add(a,b);
+    std::cout<<"The result = "<<result<<std::endl;
+    return 0;
 }
