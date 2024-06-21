f82b7fdddf3785e551603263dfa5222ce58ce0f9
FriedMomos <tanish054@outlook.com>
2024-06-21 12:37:50 +0530

    new build

--- learn/new.cpp
+++ learn/new.cpp
@@ -5,16 +5,12 @@
 using namespace std;
 
 int main(){
-<<<<<<< HEAD
-	int a=5, b=4, c=18;
-=======
 	int a=5, b=4, c=27;
->>>>>>> edit-makefile
 	std::cout<<"The sum of the number is: "<<a+b+c<<std::endl;
-	int math::add(int a, int b){
-	return (a+b);
-	}
-	int math::sub(int a, int b){
-	return (a-b);
-	}
+	
+	
+	
+	
+	return 0;
+	
 }
