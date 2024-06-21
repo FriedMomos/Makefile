f7f248839ac118056c53b8fc94fc6c03679343df
FriedMomos <70747346+FriedMomos@users.noreply.github.com>
2024-06-21 15:57:36 +0530

    Including c and d

--- learn/main.cpp
+++ learn/main.cpp
@@ -4,8 +4,10 @@
 
 int main(){
 int a,b;
+  int c,d;
 std::cin>>a>>b;
+  std::cin>>c>>d;
 std::cout<<"The result = "<<add(a,b)<<std::endl;
-std::cout<<"The Diff = "<<sub(a,b)<<std::endl;
+std::cout<<"The Diff = "<<sub(c,d)<<std::endl;
 return 0;
 }
