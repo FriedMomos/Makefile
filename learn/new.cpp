1e0cacc426aaf6d4faae61acc70e14069689a6ba
FriedMomos <tanish054@outlook.com>
2024-06-21 14:32:12 +0530

    subtraction

--- learn/new.cpp
+++ learn/new.cpp
@@ -6,10 +6,7 @@ using namespace std;
 
 int main(){
 	int a=5, b=4, c=27;
-	std::cout<<"The sum of the number is: "<<a+b+c<<std::endl;
-	
-	
-	
+	std::cout<<"The sum of the number is: "<<a-b-c<<std::endl;
 	
 	return 0;
 	
